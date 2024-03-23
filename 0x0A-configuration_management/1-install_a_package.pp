afe58b454c675bb90ec0d8740aeb7daf3a6b5e0c
Hamid <Bouayadihamid@gmail.com>
2024-03-22 20:05:21 -0700

    changed the code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-#Using Puppet, install flask from pip3
+# Install Flask package
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
+
+# Install Werkzeug package
+package { 'Werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip3',
+}
+
