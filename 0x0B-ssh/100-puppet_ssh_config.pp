79d691ab4a3f7770402868fe64c6e57a1df042b7
Hamid <Bouayadihamid@gmail.com>
2024-03-24 11:02:21 -0700

    all

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Puppet configuration script
+file_line {'PasswordAuthentication':
+  ensure => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => 'PasswordAuthentication no',
+  match    => '^PasswordAuthentication',
+  provider => puppet,
+}
+
+file_line {'IdentityFile':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => 'IdentityFile ~/.ssh/school',
+  match    => '^IdentityFile',
+  provider => puppet,
+}
